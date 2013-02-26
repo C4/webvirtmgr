a7e9caf101b4728d98214d062bbe29cc33537ac1
Carl Krauss <carl@simplymeasured.com>
2013-02-26 11:22:06 -0800

    Adding new model for and instance.

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Host(models.Model):
-    hostname = models.CharField(max_length=12)
+    hostname = models.CharField(max_length=20)
     ipaddr = models.IPAddressField()
     login = models.CharField(max_length=12)
     passwd = models.CharField(max_length=20)
@@ -15,6 +15,19 @@ class Vm(models.Model):
     host = models.ForeignKey(Host)
     vname = models.CharField(max_length=12)
     vnc_passwd = models.CharField(max_length=12)
+    def __unicode__(self):
+        return self.vname
 
+class Instance(models.Model):
+    hostname = models.CharField(max_length=20)
+    ipaddr = models.IPAddressField()
+    architecture = models.CharField(max_length=8)
+    instanceid = models.CharField(max_length=12)
+    fqdn = models.CharField(max_length=64)
+    cpu = models.CharField(max_length=8)
+    mem = models.CharField(max_length=16)
+    state = models.CharField(max_length=16)
+    location = models.CharField(max_length=32)
+    connection_type = models.CharField(max_length=20)
     def __unicode__(self):
         return self.vname
